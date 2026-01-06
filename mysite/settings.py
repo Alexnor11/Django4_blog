4e3025bf8cc940b547c7d05750c1a02ec806a700
Alexnor11 <advance114@gmail.com>
2026-01-06 13:54:24 +0300

    Creating a PostgreSQL database

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BD',
+        'USER': 'Alexnor',
+        'PASSWORD': '123456'
     }
 }
 
